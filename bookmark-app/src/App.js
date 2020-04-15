e8657d73ac65a9e8622ed645127ebbc93f5bc86b
John Azzaro <jazzaro@temple.edu>
2020-04-15 13:54:13 -0700

    implemented history method route back, imp w/rt

--- bookmark-app/src/App.js
+++ bookmark-app/src/App.js
@@ -77,10 +77,9 @@ class App extends Component {
            <Route
             path='/add-bookmark'
             render={({ history }) => {
-              console.log(history)
               return <AddBookmark
                 onAddBookmark={this.addBookmark}
-                onClickCancel={() => {/* what here? */}}
+                onClickCancel={() => history.push('/')}
               />
             }}
             />
